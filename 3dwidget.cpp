f7f4b0c50cd749009fbd938eca2b89e64eb6126f
Oliver Lau <oliver@von-und-fuer-lau.de>
2012-11-14 10:40:11 +0100

    Face-Culling ausgeschaltet, damit man die Tafel auch von hinten betrachten kann ;-)

--- 3dwidget.cpp
+++ 3dwidget.cpp
@@ -99,7 +99,6 @@ void ThreeDWidget::initializeGL(void)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
-    glEnable(GL_CULL_FACE);
     glEnable(GL_DEPTH_TEST);
 #ifndef USE_SHADER
     glEnable(GL_TEXTURE_2D);
@@ -157,7 +156,6 @@ void ThreeDWidget::paintGL(void)
     mShaderProgram->setAttributeArray(PROGRAM_TEXCOORD_ATTRIBUTE, mTexCoords);
 #endif
 
-//    glBindTexture(GL_TEXTURE_2D, mTextureHandle);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
